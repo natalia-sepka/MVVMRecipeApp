67c1ab1e519c295eaaad98fa8fca9f5a3d4e4839
natalia.sepka <natalia.sepka@gmail.com>
2023-05-06 21:12:00 +0200

    Column Layouts

--- app/src/main/java/pl/sepka/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/pl/sepka/mvvmrecipeapp/MainActivity.kt
@@ -3,13 +3,23 @@ package pl.sepka.mvvmrecipeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import pl.sepka.mvvmrecipeapp.ui.theme.MVVMRecipeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Screen()
                 }
             }
         }
@@ -34,10 +44,49 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun Screen() {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(color = Color(0xFFF2F2F2))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.happy_meal_small),
+            contentDescription = "Happy meal image",
+            modifier = Modifier.height(300.dp),
+            contentScale = ContentScale.Crop
+        )
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(
+                text = "Happy Meal",
+                style = TextStyle(fontSize = 26.sp)
+            )
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Text(
+                text = "800 Calories",
+                style = TextStyle(fontSize = 17.sp)
+            )
+            Spacer(modifier = Modifier.padding(top = 10.dp))
+            Text(
+                text = "$5.99",
+                style = TextStyle(
+                    color = Color(0xFF85bb65),
+                    fontSize = 17.sp
+                )
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MVVMRecipeAppTheme {
-        Greeting("Android")
+        Screen()
     }
 }
