d24b34f38c776034adf6d6962268374bdabbd76a
natalia.sepka <natalia.sepka@gmail.com>
2023-05-06 21:38:07 +0200

    Row Layouts

--- app/src/main/java/pl/sepka/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/pl/sepka/mvvmrecipeapp/MainActivity.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -62,23 +64,35 @@ fun Screen() {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
-            Text(
-                text = "Happy Meal",
-                style = TextStyle(fontSize = 26.sp)
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = "Happy Meal",
+                    style = TextStyle(fontSize = 26.sp)
+                )
+                Text(
+                    text = "$5.99",
+                    style = TextStyle(
+                        color = Color(0xFF85bb65),
+                        fontSize = 17.sp
+                    ),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
             Spacer(modifier = Modifier.padding(top = 10.dp))
             Text(
                 text = "800 Calories",
                 style = TextStyle(fontSize = 17.sp)
             )
             Spacer(modifier = Modifier.padding(top = 10.dp))
-            Text(
-                text = "$5.99",
-                style = TextStyle(
-                    color = Color(0xFF85bb65),
-                    fontSize = 17.sp
-                )
-            )
+            Button(
+                onClick = {},
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "ORDER NOW")
+            }
         }
     }
 }
