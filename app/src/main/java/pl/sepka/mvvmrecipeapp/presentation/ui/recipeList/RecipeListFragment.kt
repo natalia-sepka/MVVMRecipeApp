fc7507164e820ab7080c5c5fb88f99056570091c
natalia.sepka <natalia.sepka@gmail.com>
2023-06-06 13:21:50 +0200

    Chips

--- app/src/main/java/pl/sepka/mvvmrecipeapp/presentation/ui/recipeList/RecipeListFragment.kt
+++ app/src/main/java/pl/sepka/mvvmrecipeapp/presentation/ui/recipeList/RecipeListFragment.kt
@@ -24,6 +24,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
@@ -35,6 +36,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import pl.sepka.mvvmrecipeapp.R
+import pl.sepka.mvvmrecipeapp.presentation.components.FoodCategoryChip
 import pl.sepka.mvvmrecipeapp.presentation.components.RecipeCard
 
 @AndroidEntryPoint
@@ -57,7 +59,7 @@ class RecipeListFragment : Fragment() {
                 Column {
                     Surface(
                         modifier = Modifier.fillMaxWidth(),
-                        color = MaterialTheme.colors.primary,
+                        color = Color.White,
                         elevation = 8.dp
                     ) {
                         Column {
@@ -99,11 +101,12 @@ class RecipeListFragment : Fragment() {
                                     .fillMaxWidth()
                             ) {
                                 for (category in getAllFoodCategories()) {
-                                    Text(
-                                        text = category.value,
-                                        style = MaterialTheme.typography.body2,
-                                        color = MaterialTheme.colors.secondary,
-                                        modifier = Modifier.padding(8.dp)
+                                    FoodCategoryChip(
+                                        category = category.value,
+                                        onExecuteSearch = {
+                                            viewModel.onQueryChanged(it)
+                                            viewModel.newSearch(it)
+                                        }
                                     )
                                 }
                             }
